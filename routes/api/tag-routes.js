f8f21820a1761d2573398cd421744f2737262c44
Coderinsightmaven <lmarincik@proton.me>
2023-10-25 21:38:10 -0600

    fixed the categories not having a GET route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,13 +4,25 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product]
+  })
+  .then(tags => res.json(tags))
+  .catch(err => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [Product]
+  })
+  .then(tag => {
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.json(tag);
+  })
+  .catch(err => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
